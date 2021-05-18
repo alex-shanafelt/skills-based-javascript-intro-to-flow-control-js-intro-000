b57ccf4cc8f9e260650aa7d76a31d3eb621abc74
Alex Shanafelt <alexpshanafelt@gmail.com>
2021-05-18 16:17:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,38 @@
 function basicTeenager(age) {
-
+if (age >=13 && <= 19) {
+  console.log("You are a teenager!")
+}
 }
 
 function teenager(age) {
-
+  if (age >=13 && <= 19) {
+    console.log("You are a teenager!")
+  }
+  else {
+    console.log("You are not a teenager")
+  }
 }
 
 function ageChecker(age) {
-
+if (age <= 12) {
+console.log("You are a kid")
+} else if (age >= 13 && age <= 19) {
+console.log("You are a teenager!")
+} else (age >= 20) {
+console.log("You are a grownup")
+}
 }
 
 function ternaryTeenager(age) {
-
+return (age>=13 && age<=19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  switch (age) {
+  case (age>=13 && age<=19) :
+  console.log("You are a teenager");
+  break;
+  default:
+  console.log("You have an age");
+}
 }
