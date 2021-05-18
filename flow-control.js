52673b1572f3dc561eca58b1e51cf53623dc1867
Alex Shanafelt <alexpshanafelt@gmail.com>
2021-05-18 16:27:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,9 +32,9 @@ return (age>=13 && age<=19) ? "You are a teenager" : "You are not a teenager"
 function switchAge(age) {
   switch (age) {
   case (age>=13 && age<=19) :
-  console.log("You are a teenager");
+  return "You are a teenager";
   break;
   default:
-  console.log("You have an age");
+ return "You have an age";
 }
 }
